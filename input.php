7172db1e683ea2964ef3e7f9398538a245787703
hitto-ubu-dev <68594072+hitto-hub@users.noreply.github.com>
2022-11-11 15:42:04 +0900

    インデント追加

--- input.php
+++ input.php
@@ -3,11 +3,11 @@
     if (empty($_SESSION["data"])) {
         // $_SESSION["data"]が空の時実行
         echo <<<DATA
-        <form action="./judge.php" method="POST">
-        <p><b>名前</b></p>
-        <input type="text" name="name" placeholder="例）山田太郎">
-        <input type="submit" value="送信する">
-        </form>
+            <form action="./judge.php" method="POST">
+            <p><b>名前</b></p>
+            <input type="text" name="name" placeholder="例）山田太郎">
+            <input type="submit" value="送信する">
+            </form>
         DATA;
     }
 ?>
