e958eeae50b8a8e0379ac8b32f9a80ecc2a2751a
“bath” <68594072+hitto-hub@users.noreply.github.com>
2023-02-27 23:53:17 +0000

    レスポンシブ対応

--- success.php
+++ success.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="../assets/bootstrap/css/main.css">
+    <!-- <title>Document</title> -->
+</head>
+<body>
 <?php
     require_once 'function.php'; // functionファイル読み込み
     require_once 'conf.php'; // confファイル読み込み
@@ -40,5 +51,92 @@
         header('Location: ./reset.php');
         exit;
     }
-    require_once 'show.php'; // showファイル読み込み
+
+    // showだったもの
+    echo <<<date
+    <h2>記録されたデータ</h2>
+    <p>今日入った人</p>
+    date;
+    try{
+        // (1)接続
+        $db = new PDO("mysql:host=$dbhost;dbname=$dbname", "$dbusername", "$dbpassword");
+        // (2) SQLクエリ作成
+        $time = date('Y-m-d');
+        $stmt = $db->prepare("SELECT * FROM $dbtable WHERE time LIKE '$time %';");
+        // (3) SQLクエリ実行
+        $res = $stmt->execute();
+        if($res) {
+            // (4) 該当するデータを取得
+            $all = $stmt->fetchAll();
+            
+            echo  <<< date
+            <div class="table-responsive">
+            <table class="table align-middle">
+                <tr>
+                <th>id</th>
+                <th>name</th>
+                <th>time</th>
+                </tr>
+            date;
+            foreach($all as $loop){
+                // (5) 結果を表示
+                echo "<tr>";
+                echo "<td>" . $loop["id"] . "&nbsp;&nbsp;&nbsp;" . "</td>";
+                echo "<td>" .$loop["name"] . "&nbsp;&nbsp;&nbsp;" . "</td>";
+                echo "<td>" .$loop["time"] . "&nbsp;&nbsp;&nbsp;" . "</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+            echo "</div>";
+        }
+        // 切断
+        $db = null;
+    } catch(PDOException $e){
+        echo "データベース接続失敗<br>";
+        echo $e->getMessage();
+    }
+
+    // 一覧表示
+    echo <<<date
+    <p>入った人履歴</p>
+    date;
+    try{
+        // (1)接続
+        $db = new PDO("mysql:host=$dbhost;dbname=$dbname", "$dbusername", "$dbpassword");
+        // (2) SQLクエリ作成
+        $stmt = $db->prepare("SELECT * FROM $dbtable;");
+        // (3) SQLクエリ実行
+        $res = $stmt->execute();
+        if($res) {
+            // (4) 該当するデータを取得
+            $all = $stmt->fetchAll();
+            echo  <<< date
+            <div class="table-responsive">
+            <table class="table align-middle">
+                <tr>
+                <th>id</th>
+                <th>name</th>
+                <th>time</th>
+                </tr>
+            date;
+            foreach($all as $loop){
+                // (5) 結果を表示
+                echo "<tr>";
+                echo "<td>" . $loop["id"] . "&nbsp;&nbsp;&nbsp;" . "</td>";
+                echo "<td>" .$loop["name"] . "&nbsp;&nbsp;&nbsp;" . "</td>";
+                echo "<td>" .$loop["time"] . "&nbsp;&nbsp;&nbsp;" . "</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+            echo "</div>";
+        }
+        // 切断
+        $db = null;
+    } catch(PDOException $e){
+        echo "データベース接続失敗<br>";
+        echo $e->getMessage();
+    }
+
 ?>
+</body>
+</html>
